35182fe2a3f14b0cae250eb81e1b6ec48a0a9644
steffFrank <--replace-all>
2022-12-23 22:52:12 +0100

    feat: Add header

--- src/App.js
+++ src/App.js
@@ -2,16 +2,22 @@ import { Home } from "./routes/home/Home";
 import { Routes, Route } from "react-router-dom";
 import { Shop } from "./routes/shop/Shop";
 import { Navigation } from "./routes/navigation/Navigation";
+import "./App.scss";
 
 const App = () => {
+  const links = [
+    { name: "shop", text: "shop" },
+    { name: "contact" , text: "contact"},
+    { name: "signin", text: "signin" },
+  ];
   return (
     <Routes>
-      <Route path="/" element={<Navigation />}>
+      <Route path="/" element={<Navigation links={links} />}>
         <Route path="/" element={<Home />} />
         <Route path="shop" element={<Shop />} />
       </Route>
     </Routes>
-  )
-}
+  );
+};
 
 export default App;
--- src/routes/navigation/Navigation.js
+++ src/routes/navigation/Navigation.js
@@ -1,16 +1,23 @@
 import { Link, Outlet } from "react-router-dom";
+import { ReactComponent as Logo } from "../../assets/images/crown.svg";
+import "../../assets/images/crown.svg";
+import "./Navigation.scss";
 
-export const Navigation = () => {
+export const Navigation = ({links}) => {
+    const navMenu = links.map((link, index) => {
+        return <Link key={index} to={link.name} className="nav__link" ><li>{link.text}</li></Link>
+    })
+
     return (
         <>
             <header className="header">
                 <Link to="/" className="header__logo">
-                    <div>Logo</div>
+                    <Logo className="logo"/>
                 </Link>
-                <nav className="nav">
-                    <Link to="/shop" className="nav__link">
-                        Shop
-                    </Link>
+                <nav>
+                    <ul className="nav">
+                        {navMenu}
+                    </ul>
                 </nav>
             </header>
             <Outlet />
