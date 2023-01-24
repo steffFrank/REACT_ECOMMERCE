d5d6c49911ac1f1e6135e9ba59fd72b9b3df3e31
steffFrank <--replace-all>
2023-01-24 17:14:55 +0100

    feat: Use reselect to memoize the products

--- src/store/categories/category.selector.js
+++ src/store/categories/category.selector.js
@@ -1,7 +1,16 @@
-export const selectCategories = state => 
-state.categories.categories.reduce((acc, category) => {
+import { createSelector } from "reselect";
+
+
+const selectCategoryReducer = state => state.categories;
+
+const selCategories = createSelector(
+    [selectCategoryReducer],
+    (categoriesSlice) => categoriesSlice.categories
+)
+export const selectCategories = createSelector([selCategories],
+    (categories) => categories.reduce((acc, category) => {
       const {title, items} = category;
       acc[title.toLowerCase()] = items;
       return acc;
-    }, {});
+    }, {}));
 
