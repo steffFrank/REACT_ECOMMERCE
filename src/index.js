31f0b1171e64e94dcc542388d953d50c265edc22
steffFrank <--replace-all>
2023-03-29 10:42:37 +0200

    feat: Add stripe credentials

--- src/index.js
+++ src/index.js
@@ -1,15 +1,17 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import "./reset.css";
-import { App } from "./App";
-import reportWebVitals from "./reportWebVitals";
 import { BrowserRouter } from "react-router-dom";
 import { Provider } from "react-redux";
-import { store } from "./store/store";
 import { PersistGate } from "redux-persist/integration/react";
-import { persistor } from "./store/store";
 import { Loading } from "./Components/Loading/Loading";
+import { Elements } from "@stripe/react-stripe-js";
 
+import { App } from "./App";
+import { store, persistor } from "./store/store";
+import reportWebVitals from "./reportWebVitals";
+
+import "./reset.css";
+import { stripePromise } from "./utils/stripe/stripe.utils";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
@@ -17,7 +19,9 @@ root.render(
     <Provider store={store}>
       <PersistGate loading={<Loading />} persistor={persistor}>
         <BrowserRouter>
-                <App />
+        <Elements stripe={stripePromise}>
+              <App />
+        </Elements>
         </BrowserRouter>
       </PersistGate>
     </Provider>
